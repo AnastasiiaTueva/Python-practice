8d53658a45cb8ca52d24877c2a5c35be1e742a0b
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-07-29 19:19:10 +0300

    Update morse_code.py
    Added support for translating text to Morse code.

--- morse_code.py
+++ morse_code.py
@@ -27,10 +27,22 @@ morse = {
     "--..": "Z",
 
 }
+choice = input("Do you need to translate your text into morse code or morse code to text?(text/morse): ")
+if choice == "text":
+    
+    text = input("Your text: ").upper()
+    text_to_morse = { v: k for k, v in morse.items()}
+    for letter in text:
+        if letter == " ":
+            print("/", end=" ")
+        else:
+            print(text_to_morse[letter], end = " ")
 
-morse_code = input("Morse code: ")
+elif choice == "morse":
 
-splitted_morse = morse_code.split()
+    morse_code = input("Morse code: ")
 
-for letter in splitted_morse:
-    print(morse[letter], end=" ")
+    splitted_morse = morse_code.split()
+
+    for letter in splitted_morse:
+        print(morse[letter], end=" ")
